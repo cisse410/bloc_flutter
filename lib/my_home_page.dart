62554e291372461ac7c72a573ba0c05bed7a7b57
cisse410 <dramisse90@gmail.com>
2024-05-28 01:53:47 +0000

    first commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,11 @@
+import 'package:bloc_flutter/main.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
